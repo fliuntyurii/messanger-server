ab9ea7b48ac4a3b8535dbfefe5a3f2153c96b7d4
Yurii <88083323+fliuntyurii@users.noreply.github.com>
2023-03-30 13:58:34 +0300

    secret

--- app.js
+++ app.js
@@ -59,8 +59,6 @@ app.use(fileUpload());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', `${process.env.CLIENT_URL}`);
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('SameSite', 'none');
   next();
 });
 
