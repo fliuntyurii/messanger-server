757e9734262d0508d6408b1f8fb0b5b0d2904253
Yurii <88083323+fliuntyurii@users.noreply.github.com>
2023-03-31 16:22:01 +0300

    cookie

--- utils/jwt.js
+++ utils/jwt.js
@@ -16,16 +16,14 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
 
   res.cookie('accessToken', accessTokenJWT, {
     httpOnly: false,
-    signed: true,
+    // signed: true,
     expires: new Date(Date.now() + shortExp),
-    sameSite: 'Lax',
   });
 
   res.cookie('refreshToken', refreshTokenJWT, {
     httpOnly: false,
-    signed: true,
+    // signed: true,
     expires: new Date(Date.now() + longerExp),
-    sameSite: 'Lax',
   });
 };
 
